49b4bbaed624af5a76591d9e68b367f01e084872
hanul-cha <ckgks3014@naver.com>
2021-09-12 14:37:38 +0900

    express로 서버 띄우기 & 라우터 빌드

---
+++ bin/www.js
@@ -0,0 +1,7 @@
+const app = require("../server");
+
+const PORT = process.env.PORT || 5000; //환경변수로 셋팅해 env폴더의 토트값을 사용하고 없으면 3000으로 실행하겠다는 의미
+
+app.listen(PORT, () => { 
+    console.log(`서버가동 포트: ${PORT}`);
+});
