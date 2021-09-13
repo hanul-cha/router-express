40e560fce3423a46ee84ff1a8c6568686f6db2fa
hanul-cha <ckgks3014@naver.com>
2021-09-13 20:46:14 +0900

    번외로 해본 테스트

--- server.js
+++ server.js
@@ -2,11 +2,13 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
 const fs = require("fs");
+const parsing = require("./test");
 
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+console.log(parsing);
 
 const data = fs.readFileSync("./src/config/db.json");
 const conf = JSON.parse(data);
---
+++ test.js
@@ -0,0 +1,36 @@
+const axios = require("axios");
+const cheerio = require("cheerio");
+
+const getHTML = async(keyword) => {
+    try {
+        return await axios.get("https://lostark.game.onstove.com/Profile/Character/" + encodeURI(keyword))
+    }catch(err) {
+        console.log(err);
+    }
+}
+
+const parsing = async (keyword) => {
+    const html = await getHTML(keyword);
+    const $ = cheerio.load(html.data);
+    const $userInfo = $(".profile-info");
+    
+
+    let info = [];
+    $userInfo.each((idx, node) => {
+        const level = $(node).find(".level-info2__expedition span").text();
+        
+        info.push(
+            {
+                name: keyword,
+                level: $(node).find(".level-info2__expedition span").text(),
+                style: $(node).find(".game-info__title span").text(),
+
+            }
+        )  
+    });
+    return info
+}
+
+console.log(parsing("한울아잘하자"));
+
+module.exports = parsing
